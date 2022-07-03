e9f849e3d32751694b6a27e55c377f052813a52e
Makoto Hamada <ec2-user@ip-172-31-33-80.ec2.internal>
2022-07-03 23:49:37 +0000

    [Finish]応用課題５

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,20 @@ class Book < ApplicationRecord
   
   has_one_attached :image # 一つの添付物を持つ！！
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
